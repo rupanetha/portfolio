7afae17b66fc93701b3ad768d904fb83c453fb98
Sai Kranthi <iamsaikranthi@gmail.com>
2023-01-08 19:35:27 +0530

    refactor: add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import ReactGA from 'react-ga';
 
 import { HomePage, ContactPage, BlogPage, BlogDetailsPage } from './pages';
 
+const TRACKING_ID = 'UA-253360499-1'; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
+
 function App() {
+  React.useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
