6abdf21834243980270a055845e0a44d98d5ff2f
Sai Kranthi <iamsaikranthi@gmail.com>
2024-01-21 19:26:49 +0530

    remove old portfolio

--- src/components/Loading/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as AboutBlockLoader } from './About';
-export { default as ExpertiseCardLoader } from './ExpertiseCard';
-export { default as WorkBlockLoader } from './WorkBlock';
-export { default as ArticleCardLoader } from './ArticleCard';
